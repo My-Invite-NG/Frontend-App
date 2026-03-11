2b52836dafd842c7bbebc1993f25e7cbe4bd67ba
Divinegift Adesiyan <divinegiftadesiyan@gmail.com>
2026-03-11 10:19:57 +0100

    feat: Add Cloudinary and ImageKit to Next.js image remote patterns.

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: "http",
+        hostname: "localhost",
       },
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
+        protocol: "http",
+        hostname: "127.0.0.1",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
       },
     ],
   },
